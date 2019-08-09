4bc4508af5f87f3b629f0ea9d2ec4598f4c7b282
Atle Magnussen <atlemagnussen@gmail.com>
2019-08-09 20:59:48 +0200

    add the fetch colors script

---
+++ colors/get-colors.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "downloading colors"
+wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
+wget https://raw.githubusercontent.com/sickill/vim-sunburst/master/colors/Sunburst.vim
+wget https://raw.githubusercontent.com/bluz71/vim-moonfly-colors/master/colors/moonfly.vim
