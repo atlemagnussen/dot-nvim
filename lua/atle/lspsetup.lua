256e02e4f95291cda12434ef33929935ee68c9b0
Atle Magnussen <atlemagnussen@gmail.com>
2021-12-21 07:19:46 +0100

    change omnisharp dir

--- lua/atle/lspsetup.lua
+++ lua/atle/lspsetup.lua
@@ -1,6 +1,6 @@
 local pid = vim.fn.getpid()
 
-local omnisharp_bin = "/home/atle/bin/omnisharp/1.37.12/run"
+local omnisharp_bin = "/home/atle/bin/omnisharp/1.38.0/OmniSharp"
 
 local nvim_lsp = require'lspconfig'
 
